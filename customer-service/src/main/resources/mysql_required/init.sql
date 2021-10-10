0c3d2a014679e9f2a96833153b00ea11c6e693ca
zodgevaibhav <zodgevaibhav@gmail.com>
2021-10-10 21:08:01 +0530

    Deploy service using kubernetese

--- customer-service/src/main/resources/mysql_required/init.sql
+++ customer-service/src/main/resources/mysql_required/init.sql
@@ -1,5 +1,7 @@
 create database customerdb;
 use customerdb;
-create user 'vzodge'@'%' identified by 'password123';
-GRANT ALL PRIVILEGES ON *.* TO 'vzodge'@'%'with grant option;
-flush privileges;
+#drop user vzodge@localhost;
+#flush privileges;
+#create user 'vzodge'@'%' identified by 'password123';
+#GRANT ALL PRIVILEGES ON *.* TO 'vzodge'@'%'with grant option;
+#flush privileges;
