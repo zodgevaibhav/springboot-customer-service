7bc59b1c3c6248fbe62c9b3feae44180f4cb3482
Vaibhav Personal <vaibhavzodge@Vaibhavs-MacBook-Pro.local>
2022-07-17 23:46:43 +0530

    Drop user before creating - mysql bug

--- customer-service/src/main/resources/mysql_required/init.sql
+++ customer-service/src/main/resources/mysql_required/init.sql
@@ -1,5 +1,6 @@
 create database customerdb;
 use customerdb;
+drop user vzodge;
 flush privileges;
 create user 'vzodge'@'%' identified by 'password123';
 GRANT ALL PRIVILEGES ON *.* TO 'vzodge'@'%'with grant option;
